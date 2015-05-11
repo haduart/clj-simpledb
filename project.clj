fe341a0fc69585a867da80ab96937af93b62a41b
haduart <haduart@gmail.com>
2015-05-11 14:39:29 +0200

    deploy version 0.1.10 and setting new snapshot to 0.1.11

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject simpledb "0.1.10-SNAPSHOT"
+(defproject simpledb "0.1.11-SNAPSHOT"
   :description "Simple SQL database access"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
